150a4a92467b43794ce104f17df391dcafa3e390
Martin PELCAT <martinpelcat@hotmail.com>
2020-10-24 13:05:53 +0200

    continue calendat

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -11,93 +11,125 @@ export default function Calendar() {
             className="object-cover object-center h-full"
           />
         </div>
-        <div className="w-3/4 bg-white p-10 flex flex-col">
-          {/* Header */}
-          <div className="p-10">col</div>
-          {/* Calendar rows */}
-          <div className="bg-teal-200 flex-1 flex rounded-md border-collapse">
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
-              1
+        <div className="flex flex-1 flex-col h-full">
+          <div className="px-10 py-5 text-6xl font-thin tracking-widest">Janvier</div>
+          <div className="h-full bg-white px-10 pb-10 flex flex-col">
+            {/* Header */}
+            {/* Calendar rows */}
+            <div className="flex-1 flex rounded-md">
+              <div className="w-1/7 h-full bg-white border-t border-l rounded-tl-xl text-gray-300 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
+                30
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
+                31
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                1
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                2
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                3
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                4
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-r rounded-tr-xl text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                5
+              </div>
             </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              2
+            <div className="flex-1 flex rounded-md">
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
+                6
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                7
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                8
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                9
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                10
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                11
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-r text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                12
+              </div>
             </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              3
+            <div className="flex-1 flex rounded-md">
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
+                13
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                14
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                15
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                16
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                17
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                18
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-r text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                19
+              </div>
             </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              4
+            <div className="flex-1 flex rounded-md">
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
+                20
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                21
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                22
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                23
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                24
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                25
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-r text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                26
+              </div>
             </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              5
-            </div>
-          </div>
-          <div className="bg-teal-200 flex-1 flex rounded-md">
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              1
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              2
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              3
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              4
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              5
-            </div>
-          </div>{" "}
-          <div className="bg-teal-200 flex-1 flex rounded-md ">
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              1
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              2
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              3
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              4
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              5
-            </div>
-          </div>{" "}
-          <div className="bg-teal-200 flex-1 flex rounded-md">
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              1
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              2
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              3
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              4
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              5
-            </div>
-          </div>{" "}
-          <div className="bg-teal-200 flex-1 flex rounded-md">
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              1
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              2
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              3
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              4
-            </div>
-            <div className="w-1/5 h-full bg-white border border-1 text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
-              5
+            <div className="flex-1 flex rounded-md">
+              <div className="w-1/7 h-full bg-white border-t border-l border-b rounded-bl-xl text-gray-500 border-collapse text-4xl text-right p-2 leading-5 border-gray-300">
+                27
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                28
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                29
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                30
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b text-gray-500 text-4xl text-right p-2 leading-5 border-gray-300">
+                31
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
+                1
+              </div>
+              <div className="w-1/7 h-full bg-white border-t border-l border-b border-r rounded-br-xl text-gray-300 text-4xl text-right p-2 leading-5 border-gray-300">
+                2
+              </div>
             </div>
           </div>
         </div>
